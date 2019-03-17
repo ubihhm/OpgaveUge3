2e38ccad89176734137285d93b7938a9a7a0c28d
ubihhm <48522269+ubihhm@users.noreply.github.com>
2019-03-17 10:29:38 +0100

    Add files via upload

---
+++ OpgaveUge3/Models/Vurdering.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpgaveUge3.Models
+{
+    public class Vurdering
+    {
+        [Required()]
+        public string Navn { get; set; }
+        [Required()]
+        public string Produkt { get; set; }
+        [Required(), DataType(DataType.MultilineText)]
+        public string Beskrivelse { get; set; }
+        [Required()]
+        public int Karakter { get; set; }
+
+    }
+}
